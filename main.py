96649a0500d6f6d3a126cefec23ff84cbb76204a
Sean Hupe <seanhupe@yahoo.com>
2024-09-01 15:39:47 -0700

    commit

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ while is_on:
         if is_resource_sufficient(drink["ingredients"]):
             payment = process_coins()
             is_transaction_successful(payment, drink["cost"])
+            make_coffee(choice, drink["ingredients"])
 
 
 
