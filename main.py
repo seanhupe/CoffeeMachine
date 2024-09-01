0cc85a9b6e18107d8e442275b9ddd4aca44342bb
Sean Hupe <seanhupe@yahoo.com>
2024-09-01 15:20:49 -0700

    commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from functools import total_ordering
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -24,6 +26,25 @@ MENU = {
     }
 }
 
+def is_resource_sufficient(order_ingredients):
+    """Returns True if order can be made, False when ingredients are insufficient"""
+    enough_resources = True
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough water {item}.")
+            enough_resources = False
+    return enough_resources
+
+
+def process_coints():
+    """Returns the total calculated from coins inserted"""
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickels?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
 profit = 0
 resources = {
     "water": 300,
@@ -43,5 +64,10 @@ while is_on:
         print(f"Milk: {resources['milk']}ml")
         print(f"Coffee: {resources['coffee']}g")
         print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        print(drink)
+        if is_resource_sufficient(drink["ingredients"]):
+
 
 
